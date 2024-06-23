8f828d502a4c2b1331f2b01d2c43e95fc5d66b93
John Dott <jkd28@pitt.edu>
2024-06-23 16:47:42 -0400

    Display all logged data on front of screen

--- src/FireWatchUI/app.py
+++ src/FireWatchUI/app.py
@@ -1,4 +1,5 @@
 import faicons as fa
+import pandas as pd
 import plotly.express as px
 from pathlib import Path
 
@@ -9,5 +10,28 @@ from pathlib import Path
 from shiny import reactive, render
 from shiny.express import input, ui
 
+app_dir = Path(__file__).parent
+
 ui.page_opts(title="EVFD", fillable=True)
-ui.include_css( Path(__file__).parent / "stylesheets/styles.css")
+all_logged_data = pd.read_csv(app_dir / "../../data/LOG_ENTRIES.csv")
+
+with ui.layout_columns(fill = False):
+  with ui.card(full_screen=True):
+    ui.card_header("Logged Data")
+
+    @render.data_frame
+    def table():
+      return render.DataGrid(get_all_logged_data())
+    
+ui.include_css( app_dir / "stylesheets/styles.css")
+
+
+# --------------------------------------------------------
+# Reactive calculations and effects
+# --------------------------------------------------------
+
+
+@reactive.calc
+def get_all_logged_data():
+    return all_logged_data
+
